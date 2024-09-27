83935cce959dc423a6a6f02384a65b53945bb580
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-27 11:29:32 +0200

    Added class names to the nave code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="images/troll-face.png" alt="" />
-      <h2>Header component</h2>
-      <h4>React Course - Project 3 </h4>
+      <img src="images/troll-face.png" alt="" className="header--image" />
+      <h2 className="header--title">Header component</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
