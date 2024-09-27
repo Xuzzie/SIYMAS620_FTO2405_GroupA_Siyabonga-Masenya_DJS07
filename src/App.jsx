b646dd3000fc2b837fe39bc436bc4dc267238232
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-27 11:00:57 +0200

    Fixed connecting my components to my app Jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
-function App() {
-
+import Header from "./components/Header.jsx";
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <Header />
+    </div>
+  );
 }
-
-export default App
