e8a613e9c18023df7d01e4dc26268213dd9d86e1
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-27 12:37:51 +0200

    Had an issue with the styling was very off had to copy code from the scrimba multiple times to fix  , I actully think the problem was  with vs code itself

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,8 +2,20 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <div>
+          <label>Top Text</label>
+          <input id type="text" placeholder="Shut up" className="form--input" />
+        </div>
+        <div>
+          <label>
+            Bottom Text
+            <input
+              type="text"
+              placeholder="and take my money"
+              className="form--input"
+            />
+          </label>
+        </div>
         <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
