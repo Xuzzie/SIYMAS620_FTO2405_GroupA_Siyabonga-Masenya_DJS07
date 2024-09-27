ad006762024390b28ace87ff1879cb83f3e6e29e
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-09-27 11:51:00 +0200

    Fixed padding issue with main  class element

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,10 @@
 export default function Meme() {
   return (
     <main>
-      <form className="form" action="">
-        <input type="text" className="form--input" placeholder="Top text" />
-        <input
-          type="text"
-          className="form--input"
-          placeholder=" Bottom text "
-        />
-        <button>Get a new meme image 🖼</button>
+      <form className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
   );
